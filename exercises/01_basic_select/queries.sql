8fbc7116ea54575d4a62350c10b2d431f62ed642
Elena S. Fuchs <elena.s.fuchs@gmail.com>
2026-02-12 11:37:42 +0100

    Add SQL queries for student data analysis

--- exercises/01_basic_select/queries.sql
+++ exercises/01_basic_select/queries.sql
@@ -1,1 +1,29 @@
+SELECT * FROM students;
+
+SELECT *
+FROM students
+WHERE age IS NULL;
+
+SELECT 
+    COUNT(*) AS total_students,
+    COUNT(age) AS students_with_age
+FROM students;
+
+SELECT 
+    COUNT(*) AS total,
+    COUNT(age) AS non_null_ages,
+    COUNT(*) - COUNT(age) AS null_ages
+FROM students;
+
+SELECT 
+    grade,
+    COUNT(*) AS students_per_grade
+FROM students
+GROUP BY grade;
+
+SELECT 
+    COUNT(*) AS total,
+    COUNT(age) AS non_null_ages,
+    COUNT(grade) AS non_null_grades
+FROM students;
 
