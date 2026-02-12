a83441ebd9fecbbdb729e09038d26b81fcc6e090
Elena S. Fuchs <elena.s.fuchs@gmail.com>
2026-02-12 11:37:23 +0100

    Add insert statements for students data

--- exercises/01_basic_select/inserts.sql
+++ exercises/01_basic_select/inserts.sql
@@ -1,1 +1,9 @@
+INSERT INTO students (name, age, grade)
+VALUES 
+('Anna', 23, 'A'),
+('Ben', NULL, 'B'),
+('Clara', 25, NULL);
+
+INSERT INTO students (name, age, grade)
+VALUES ('David', NULL, NULL);
 
